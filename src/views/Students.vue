bef46a549caa07b7af2c2819cc9b35737e2c7198
Ramunas <zamrif@gmail.com>
2020-08-10 20:27:56 +0300

    student table created

--- src/views/Students.vue
+++ src/views/Students.vue
@@ -5,7 +5,8 @@
     <table class="table is-fullwidth">
       <thead>
         <tr>
-          <th>Full Name</th>
+          <th>Name</th>
+          <th>Surname</th>
           <th>Gender</th>
           <th>D.O.B</th>
           <th>Phone</th>
@@ -15,10 +16,11 @@
       <tbody>
         <tr>
           <td>Ramas</td>
+          <td>Mali</td>
           <td>M</td>
-          <td>20</td>
-          <td>454</td>
-          <td></td>
+          <td>88/09/12</td>
+          <td>0037688670</td>
+          <td>mail@email.com</td>
         </tr>
       </tbody>
     </table>
