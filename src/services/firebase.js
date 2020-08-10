a517de1deee51eca4cd7417c5bf098f10476a2f1
Ramunas <zamrif@gmail.com>
2020-08-10 18:46:01 +0300

    firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqL6KzfFq0ZrtCAYGz6tONMG2tPbxZ_Z0",
+  authDomain: "codeacademy-classes.firebaseapp.com",
+  databaseURL: "https://codeacademy-classes.firebaseio.com",
+  projectId: "codeacademy-classes",
+  storageBucket: "codeacademy-classes.appspot.com",
+  messagingSenderId: "1058640948884",
+  appId: "1:1058640948884:web:71ac2161c28093b7bd0e81",
+};
+
+firebase.initializeApp(firebaseConfig);
