a5a04bc0df6c30a51c6619bacb8c06a5e2fb9a73
Ramunas <zamrif@gmail.com>
2020-08-10 18:55:19 +0300

    Login

---
+++ src/components/Notification.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="notification" v-bind:class="type">
+    <button class="delete" @click="close"></button>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Notification",
+  props: {
+    type: String,
+    message: String,
+  },
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
