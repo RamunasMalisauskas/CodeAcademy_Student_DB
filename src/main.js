56c95a7f21d2f8c3f16cd4806bc220721bb402a8
Ramunas <zamrif@gmail.com>
2020-08-10 18:50:29 +0300

    bulma/normalize

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './services/firebase'
+import 'bulma/css/bulma.min.css'
+import 'normalize.css'
 
 Vue.config.productionTip = false
 
---
+++ src/views/Login.vue
@@ -0,0 +1,113 @@
+
+<template>
+  <div class="login">
+    <h2 class="title">Login</h2>
+
+    <form name="iki" v-on:submit.prevent="login">
+      <label for="email">Email</label>
+      <input
+        type="email"
+        v-model="email"
+        id="email"
+        name="email"
+        placeholder="youremail@email.lt"
+        required
+      />
+
+      <label for="Password">Password</label>
+      <input
+        type="password"
+        v-model="password"
+        id="password"
+        name="password"
+        placeholder="type your password here ->"
+        required
+      />
+
+      <button type="submit" class="button" :class="loading && 'is-loading'">
+        Login
+      </button>
+
+      <Notification
+        v-on:close="error = true"
+        type="error"
+        :message="errorMessage"
+      />
+    </form>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase/app";
+import "firebase/auth";
+import Notification from "../components/Notification";
+
+export default {
+  name: "Login",
+  components: { Notification },
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: false,
+      errorMessage: "",
+      loading: false,
+    };
+  },
+
+  methods: {
+    login() {
+      this.loading = true;
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(
+          () => this.$router.push("/"),
+          () => {
+            this.error = true;
+            this.errorMessage =
+              "failed to log-in, check you login email and password";
+            this.loading = false;
+          }
+        );
+    },
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  color: rgb(80, 80, 80);
+  margin: 0 0 30px 0;
+}
+
+label {
+  color: rgb(80, 80, 80);
+  display: block;
+  min-width: 100%;
+}
+
+input {
+  margin-bottom: 15px;
+  padding: 10px;
+  border: 1px solid rgb(195, 0, 255);
+  border-radius: 10px;
+  box-sizing: border-box;
+  width: 66%;
+}
+
+button {
+  display: block;
+  padding: 10px 30px;
+  border: 0;
+  border-radius: 10px;
+  background-color: rgb(195, 0, 255);
+  color: rgb(255, 255, 255);
+}
+
+button:hover {
+  cursor: pointer;
+  color: rgb(195, 0, 255);
+  background-color: rgb(255, 255, 255);
+}
+</style>
